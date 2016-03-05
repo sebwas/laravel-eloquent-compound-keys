1ca40604e3ab38ece5cdb327f480b98aa7e3ea90
Sebastian Wasser <sebastian.wasser@gmail.com>
2016-03-05 13:35:30 +0100

    Fixed bugs

--- src/SebWas/Laravel/Eloquent/CompoundKeys.php
+++ src/SebWas/Laravel/Eloquent/CompoundKeys.php
@@ -5,210 +5,210 @@ namespace SebWas\Laravel\Eloquent;
 use Illuminate\Database\Eloquent\Builder;
 
 trait CompoundKeys {
-    /**
-     * Qualifies a given key.
-     *
-     * @param  string $key
-     * @return string
-     */
-    public function qualifyKey(string $key){
-        return $this->getTable().'.'.$key;
-    }
+	/**
+	 * Qualifies a given key.
+	 *
+	 * @param  string $key
+	 * @return string
+	 */
+	public function qualifyKey(string $key){
+		return $this->getTable().'.'.$key;
+	}
 
-    /**
-     * Set the keys for a save update query.
-     *
-     * @param  \Illuminate\Database\Eloquent\Builder  $query
-     * @return \Illuminate\Database\Eloquent\Builder
-     */
-    protected function setKeysForSaveQuery(Builder $query){
-        $query->where($this->getKeyForSaveQuery());
+	/**
+	 * Set the keys for a save update query.
+	 *
+	 * @param  \Illuminate\Database\Eloquent\Builder  $query
+	 * @return \Illuminate\Database\Eloquent\Builder
+	 */
+	protected function setKeysForSaveQuery(Builder $query){
+		$query->where($this->getKeyForSaveQuery());
 
-        return $query;
-    }
+		return $query;
+	}
 
-    /**
-     * Get the primary key value for a save query.
-     *
-     * @return mixed
-     */
-    protected function getKeyForSaveQuery(){
-        return $this->getKey();
-    }
+	/**
+	 * Get the primary key value for a save query.
+	 *
+	 * @return mixed
+	 */
+	protected function getKeyForSaveQuery(){
+		return $this->getKey();
+	}
 
-    /**
-     * Get the value of the model's primary key.
-     *
-     * @return array
-     */
-    public function getKey(){
-        return array_intersect_key($this->attributes, array_flip($this->getKeyName()));
-    }
+	/**
+	 * Get the value of the model's primary key.
+	 *
+	 * @return array
+	 */
+	public function getKey(){
+		return array_intersect_key($this->attributes, array_flip($this->getKeyName()));
+	}
 
-    /**
-     * Reload a fresh model instance from the database.
-     *
-     * @param  array|string  $with
-     * @return $this|null
-     */
-    public function fresh($with = []){
-        if (! $this->exists) {
-            return;
-        }
+	/**
+	 * Reload a fresh model instance from the database.
+	 *
+	 * @param  array|string  $with
+	 * @return $this|null
+	 */
+	public function fresh($with = []){
+		if (! $this->exists) {
+			return;
+		}
 
-        return static::with($with)->where($this->getKey())->first();
-    }
+		return static::with($with)->where($this->getKey())->first();
+	}
 
-    /**
-     * Define a one-to-one relationship.
-     *
-     * @param  string  $related
-     * @param  string  $foreignKey
-     * @param  string  $localKey
-     * @return \Illuminate\Database\Eloquent\Relations\HasOne
-     */
-    public function hasOne($related, $foreignKey = null, $localKey = null) {
-        $localKey = $localKey ?: with(new $related)->getForeignKey();
+	/**
+	 * Define a one-to-one relationship.
+	 *
+	 * @param  string  $related
+	 * @param  string  $foreignKey
+	 * @param  string  $localKey
+	 * @return \Illuminate\Database\Eloquent\Relations\HasOne
+	 */
+	public function hasOne($related, $foreignKey = null, $localKey = null) {
+		$localKey = $localKey ?: with(new $related)->getForeignKey();
 
-        return parent::hasOne($related, $foreignKey, $localKey);
-    }
+		return parent::hasOne($related, $foreignKey, $localKey);
+	}
 
-    /**
-     * Define a polymorphic one-to-one relationship.
-     *
-     * @param  string  $related
-     * @param  string  $name
-     * @param  string  $type
-     * @param  string  $id
-     * @param  string  $localKey
-     * @return \Illuminate\Database\Eloquent\Relations\MorphOne
-     */
-    public function morphOne($related, $name, $type = null, $id = null, $localKey = null){
-        $localKey = $localKey ?: with(new $related)->getForeignKey();
+	/**
+	 * Define a polymorphic one-to-one relationship.
+	 *
+	 * @param  string  $related
+	 * @param  string  $name
+	 * @param  string  $type
+	 * @param  string  $id
+	 * @param  string  $localKey
+	 * @return \Illuminate\Database\Eloquent\Relations\MorphOne
+	 */
+	public function morphOne($related, $name, $type = null, $id = null, $localKey = null){
+		$localKey = $localKey ?: with(new $related)->getForeignKey();
 
-        return parent::morphOne($related, $name, $type, $id, $localKey);
-    }
+		return parent::morphOne($related, $name, $type, $id, $localKey);
+	}
 
-    /**
-     * Define an inverse one-to-one or many relationship.
-     *
-     * @param  string  $related
-     * @param  string  $foreignKey
-     * @param  string  $otherKey
-     * @param  string  $relation
-     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
-     */
-    public function belongsTo($related, $foreignKey = null, $otherKey = null, $relation = null){
-        $foreignKey = $foreignKey ?: with(new $related)->getForeignKey();
+	/**
+	 * Define an inverse one-to-one or many relationship.
+	 *
+	 * @param  string  $related
+	 * @param  string  $foreignKey
+	 * @param  string  $otherKey
+	 * @param  string  $relation
+	 * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+	 */
+	public function belongsTo($related, $foreignKey = null, $otherKey = null, $relation = null){
+		$foreignKey = $foreignKey ?: with(new $related)->getForeignKey();
 
-        return parent::belongsTo($related, $foreignKey, $otherKey, $relation);
-    }
+		return parent::belongsTo($related, $foreignKey, $otherKey, $relation);
+	}
 
-    /**
-     * Define a one-to-many relationship.
-     *
-     * @param  string  $related
-     * @param  string  $foreignKey
-     * @param  string  $localKey
-     * @return \Illuminate\Database\Eloquent\Relations\HasMany
-     */
-    public function hasMany($related, $foreignKey = null, $localKey = null){
-        $localKey = $localKey ?: with(new $related)->getForeignKey();
+	/**
+	 * Define a one-to-many relationship.
+	 *
+	 * @param  string  $related
+	 * @param  string  $foreignKey
+	 * @param  string  $localKey
+	 * @return \Illuminate\Database\Eloquent\Relations\HasMany
+	 */
+	public function hasMany($related, $foreignKey = null, $localKey = null){
+		$localKey = $localKey ?: with(new $related)->getForeignKey();
 
-        return parent::hasMany($related, $foreignKey, $localKey);
-    }
+		return parent::hasMany($related, $foreignKey, $localKey);
+	}
 
-    /**
-     * Define a polymorphic one-to-many relationship.
-     *
-     * @param  string  $related
-     * @param  string  $name
-     * @param  string  $type
-     * @param  string  $id
-     * @param  string  $localKey
-     * @return \Illuminate\Database\Eloquent\Relations\MorphMany
-     */
-    public function morphMany($related, $name, $type = null, $id = null, $localKey = null){
-        $localKey = $localKey ?: with(new $related)->getForeignKey();
+	/**
+	 * Define a polymorphic one-to-many relationship.
+	 *
+	 * @param  string  $related
+	 * @param  string  $name
+	 * @param  string  $type
+	 * @param  string  $id
+	 * @param  string  $localKey
+	 * @return \Illuminate\Database\Eloquent\Relations\MorphMany
+	 */
+	public function morphMany($related, $name, $type = null, $id = null, $localKey = null){
+		$localKey = $localKey ?: with(new $related)->getForeignKey();
 
-        return parent::morphMany($related, $name, $type, $id, $localKey);
-    }
+		return parent::morphMany($related, $name, $type, $id, $localKey);
+	}
 
-    /**
-     * Run the increment or decrement method on the model.
-     *
-     * @param  string  $column
-     * @param  int  $amount
-     * @param  array  $extra
-     * @param  string  $method
-     * @return int
-     */
-    protected function incrementOrDecrement($column, $amount, $extra, $method){
-        $query = $this->newQuery();
+	/**
+	 * Run the increment or decrement method on the model.
+	 *
+	 * @param  string  $column
+	 * @param  int  $amount
+	 * @param  array  $extra
+	 * @param  string  $method
+	 * @return int
+	 */
+	protected function incrementOrDecrement($column, $amount, $extra, $method){
+		$query = $this->newQuery();
 
-        if (! $this->exists) {
-            return $query->{$method}($column, $amount, $extra);
-        }
+		if (! $this->exists) {
+			return $query->{$method}($column, $amount, $extra);
+		}
 
-        $this->incrementOrDecrementAttributeValue($column, $amount, $method);
+		$this->incrementOrDecrementAttributeValue($column, $amount, $method);
 
-        return $query->where($this->getKey())->{$method}($column, $amount, $extra);
-    }
+		return $query->where($this->getKey())->{$method}($column, $amount, $extra);
+	}
 
-    /**
-     * Insert the given attributes and set the ID on the model.
-     *
-     * @param  \Illuminate\Database\Eloquent\Builder  $query
-     * @param  array  $attributes
-     * @return void
-     */
-    protected function insertAndSetId(Builder $query, $attributes){
-        $query->insertGetId($attributes);
-    }
+	/**
+	 * Insert the given attributes and set the ID on the model.
+	 *
+	 * @param  \Illuminate\Database\Eloquent\Builder  $query
+	 * @param  array  $attributes
+	 * @return void
+	 */
+	protected function insertAndSetId(Builder $query, $attributes){
+		$query->insertGetId($attributes);
+	}
 
-    /**
-     * Get the route key for the model.
-     *
-     * @return string
-     */
-    public function getRouteKeyName(){
-        return '';
-    }
+	/**
+	 * Get the route key for the model.
+	 *
+	 * @return string
+	 */
+	public function getRouteKeyName(){
+		return '';
+	}
 
-    /**
-     * Clone the model into a new, non-existing instance.
-     *
-     * @param  array|null  $except
-     * @return \Illuminate\Database\Eloquent\Model
-     */
-    public function replicate(array $except = null){
-        $except = $except ?: array_merge($this->getKeyName(), [
-            $this->getCreatedAtColumn(),
-            $this->getUpdatedAtColumn(),
-        ]);
+	/**
+	 * Clone the model into a new, non-existing instance.
+	 *
+	 * @param  array|null  $except
+	 * @return \Illuminate\Database\Eloquent\Model
+	 */
+	public function replicate(array $except = null){
+		$except = $except ?: array_merge($this->getKeyName(), [
+			$this->getCreatedAtColumn(),
+			$this->getUpdatedAtColumn(),
+		]);
 
-        return parent::replicate($except);
-    }
+		return parent::replicate($except);
+	}
 
-    /**
-     * Define a polymorphic, inverse one-to-one or many relationship.
-     *
-     * @param  string  $name
-     * @param  string  $type
-     * @param  string  $id
-     * @return \Illuminate\Database\Eloquent\Relations\MorphTo
-     */
-    public function morphTo($name = null, $type = null, $id = null){
-        // TODO
-    }
+	/**
+	 * Define a polymorphic, inverse one-to-one or many relationship.
+	 *
+	 * @param  string  $name
+	 * @param  string  $type
+	 * @param  string  $id
+	 * @return \Illuminate\Database\Eloquent\Relations\MorphTo
+	 */
+	public function morphTo($name = null, $type = null, $id = null){
+		// TODO
+	}
 
-    /**
-     * Destroy the models for the given IDs.
-     *
-     * @param  array|int  $ids
-     * @return int
-     */
-    public static function destroy($ids){
-        // TODO
-    }
+	/**
+	 * Destroy the models for the given IDs.
+	 *
+	 * @param  array|int  $ids
+	 * @return int
+	 */
+	public static function destroy($ids){
+		// TODO
+	}
 }
